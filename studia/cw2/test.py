e3ecaaa7b1c62a93fc91f670f6487a0e88a7be02
jake-stolarsky-pc <jake.stolarsky@gmail.com>
2022-05-05 21:30:28 +0200

    finally working list of cards

--- studia/cw2/test.py
+++ studia/cw2/test.py
@@ -1,18 +1,27 @@
-
 karta_rodzaj = ["Kier", "Karo", "Trefl", "Pik"]
 karta_wartosc = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 
-talia_kier = {}
-
-
-#ma wykonac ta petle tyle razy ile jest w karta_wartosc = 13
-karty_kier = []
+key_karty_kier =[]
+key_karty_karo =[]
+key_karty_trefl =[]
+key_karty_pik =[]
 
 for i in karta_wartosc:
-    karty_kier.append("Kier")
+    key_karty_kier.append(karta_rodzaj[0])
+    key_karty_karo.append(karta_rodzaj[1])
+    key_karty_trefl.append(karta_rodzaj[2])
+    key_karty_pik.append(karta_rodzaj[3])
 
-print(karty_kier)
+talia_kier = list(zip(key_karty_kier, karta_wartosc))
+talia_karo = list(zip(key_karty_karo, karta_wartosc))
+talia_trefl = list(zip(key_karty_trefl, karta_wartosc))
+talia_pik = list(zip(key_karty_pik, karta_wartosc))
 
-talia_kier = {karty_kier[i]: karta_wartosc[i] for i in range(len(karta_wartosc))}
+talia = []
+talia.extend(talia_kier)
+talia.extend(talia_karo)
+talia.extend(talia_trefl)
+talia.extend(talia_pik)
 
-print(talia_kier)
+print(talia)
+print(len(talia))
