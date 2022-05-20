c0766ad7c13545b49727e334d4866af766a19d5d
jaki-jake <stolarczyk.jk@gmail.com>
2022-05-20 19:48:56 +0200

    cards exercise complete
    end of exercise 2.1 for collage

--- studia/cw2/cw2.1_main.py
+++ studia/cw2/cw2.1_main.py
@@ -1,37 +1,75 @@
-"""
-Zadanie 1
-1. Napisz klasę Talia, która będzie zawierać inną klasę - klasę Karta, która spałnia następujące wymagania:
-    - rodzaj (Kier, Karo, Trefl, Pik)
-    - wartość (A,2,3,4,5,6,7,8,9,10,J,Q,K)
-2. Dodaj metody:
-    - rozdaj, aby pobrać pojedynczą kartę z talii - karta po rozdaniu usuwana jest z talii
-    - przetasuj, która zapewnia że Talia składa się z 52 kart i ustawia je w sposób losowy
-"""
+import random
 
 class Talia:
-    
-    #inicjalizuj talie ze wszystkimi kartami
-    def __init__(self):
-        self.kart = self.Karta()
 
-    #pobieraj pojedyncza karte z talii po czym jest usowana z talii
-    def rozdaj():
-        pass
+    def create():
+        _karta_rodzaj = ["Kier", "Karo", "Trefl", "Pik"]
+        _karta_wartosc = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 
-    #tasuje wszystkie 52 karty i ustawia je w sposob losowy
-    def przetasuj():
-        pass
+        _key_karty_kier =[]
+        _key_karty_karo =[]
+        _key_karty_trefl =[]
+        _key_karty_pik =[]
 
-    #inner class Karta
-    class Karta:
-        
-        def __init__(self):
-            self.rodzaj = ["Kier", "Karo", "Trefl", "Pik"]
-            self.wartosc = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-        
-        def display(self):
-            print("Rodzaj: ", self.rodzaj)
-            print("Wartosc: ", self.wartosc)
+        for i in _karta_wartosc:
+            _key_karty_kier.append(_karta_rodzaj[0])
+            _key_karty_karo.append(_karta_rodzaj[1])
+            _key_karty_trefl.append(_karta_rodzaj[2])
+            _key_karty_pik.append(_karta_rodzaj[3])
+
+        _talia_kier = list(zip(_key_karty_kier, _karta_wartosc))
+        _talia_karo = list(zip(_key_karty_karo, _karta_wartosc))
+        _talia_trefl = list(zip(_key_karty_trefl, _karta_wartosc))
+        _talia_pik = list(zip(_key_karty_pik, _karta_wartosc))
+
+        _talia = []
+        _talia.extend(_talia_kier)
+        _talia.extend(_talia_karo)
+        _talia.extend(_talia_trefl)
+        _talia.extend(_talia_pik)
+        return _talia
+
+    def tasuj(talia):
+        random.shuffle(talia)
+        return talia
+
+    def rozdaj(talia):
+        _rand_numb = random.randrange(0, len(talia))
+        _karta = talia[_rand_numb]
+        talia.pop(_rand_numb)
+        return print(_karta)
+
+    def ilosc_kart(talia):
+        return print(len(talia))
+
+    def pokaz_karty(talia):
+        return print(talia)
 
 
-Talia.kart.display()
+#create talia in variable
+talia = Talia.create()
+io_program = 1
+
+while io_program == 1 :
+
+    #menu
+    user_input = input("\nMENU: \n 1. Liste kart \n 2. Ilosc kart \n 3. Potasowac karty \n 4. Wyciagnac z talii losowa karte \n 0. Zakoncz program \nTwoj wybor to: ")
+
+    #user choose
+    if (user_input == "0"):
+        print("\n..koniec programu\n")
+        io_program = 0
+    elif (user_input == "1"):
+        print("\nTwoja lista kart:")
+        Talia.pokaz_karty(talia)
+    elif (user_input == "2"):
+        print ("\nTwoja liczba kart to: ")
+        Talia.ilosc_kart(talia)
+    elif (user_input == "3"):
+        Talia.tasuj(talia)
+        print("\n..karty potasowane")
+    elif (user_input == "4"):
+        print("\nTwoja losowa karta to: ")
+        Talia.rozdaj(talia)
+    else:
+        print("\n..nie ma takiej komendy")
--- studia/cw2/test.py
+++ studia/cw2/test.py
@@ -3,42 +3,73 @@ import random
 class Talia:
 
     def create():
-        karta_rodzaj = ["Kier", "Karo", "Trefl", "Pik"]
-        karta_wartosc = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+        _karta_rodzaj = ["Kier", "Karo", "Trefl", "Pik"]
+        _karta_wartosc = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 
-        key_karty_kier =[]
-        key_karty_karo =[]
-        key_karty_trefl =[]
-        key_karty_pik =[]
+        _key_karty_kier =[]
+        _key_karty_karo =[]
+        _key_karty_trefl =[]
+        _key_karty_pik =[]
 
-        for i in karta_wartosc:
-            key_karty_kier.append(karta_rodzaj[0])
-            key_karty_karo.append(karta_rodzaj[1])
-            key_karty_trefl.append(karta_rodzaj[2])
-            key_karty_pik.append(karta_rodzaj[3])
+        for i in _karta_wartosc:
+            _key_karty_kier.append(_karta_rodzaj[0])
+            _key_karty_karo.append(_karta_rodzaj[1])
+            _key_karty_trefl.append(_karta_rodzaj[2])
+            _key_karty_pik.append(_karta_rodzaj[3])
 
-        talia_kier = list(zip(key_karty_kier, karta_wartosc))
-        talia_karo = list(zip(key_karty_karo, karta_wartosc))
-        talia_trefl = list(zip(key_karty_trefl, karta_wartosc))
-        talia_pik = list(zip(key_karty_pik, karta_wartosc))
+        _talia_kier = list(zip(_key_karty_kier, _karta_wartosc))
+        _talia_karo = list(zip(_key_karty_karo, _karta_wartosc))
+        _talia_trefl = list(zip(_key_karty_trefl, _karta_wartosc))
+        _talia_pik = list(zip(_key_karty_pik, _karta_wartosc))
 
-        talia = []
-        talia.extend(talia_kier)
-        talia.extend(talia_karo)
-        talia.extend(talia_trefl)
-        talia.extend(talia_pik)
-        return talia
-
-    def tasuj():
-        _talia = Talia.create() 
-        random.shuffle(_talia)
+        _talia = []
+        _talia.extend(_talia_kier)
+        _talia.extend(_talia_karo)
+        _talia.extend(_talia_trefl)
+        _talia.extend(_talia_pik)
         return _talia
 
-    def rozdaj():
-        _karta = Talia.tasuj()[0]
-        return _karta
+    def tasuj(talia):
+        random.shuffle(talia)
+        return talia
+
+    def rozdaj(talia):
+        _rand_numb = random.randrange(0, len(talia))
+        _karta = talia[_rand_numb]
+        talia.pop(_rand_numb)
+        return print(_karta)
+
+    def ilosc_kart(talia):
+        return print(len(talia))
+
+    def pokaz_karty(talia):
+        return print(talia)
 
 
-talia_potasowana = Talia.rozdaj()
+#create talia in variable
+talia = Talia.create()
+io_program = 1
 
-print(talia_potasowana)
+while io_program == 1 :
+
+    #menu
+    user_input = input("\nMENU: \n 1. Liste kart \n 2. Ilosc kart \n 3. Potasowac karty \n 4. Wyciagnac z talii losowa karte \n 0. Zakoncz program \nTwoj wybor to: ")
+
+    #user choose
+    if (user_input == "0"):
+        print("\n..koniec programu\n")
+        io_program = 0
+    elif (user_input == "1"):
+        print("\nTwoja lista kart:")
+        Talia.pokaz_karty(talia)
+    elif (user_input == "2"):
+        print ("\nTwoja liczba kart to: ")
+        Talia.ilosc_kart(talia)
+    elif (user_input == "3"):
+        Talia.tasuj(talia)
+        print("\n..karty potasowane")
+    elif (user_input == "4"):
+        print("\nTwoja losowa karta to: ")
+        Talia.rozdaj(talia)
+    else:
+        print("\n..nie ma takiej komendy")
